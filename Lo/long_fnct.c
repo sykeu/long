0a79a293d2196325360eea663b384775795dcf96
sykeu <92817890+sykeu@users.noreply.github.com>
2022-09-15 18:49:44 +0100

    Add files via upload

---
+++ Lo/long_fnct.c
@@ -0,0 +1,92 @@
+#include "long.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != (char)c)
+	{
+		if (s[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return (&s[i]);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		l;
+	char	*k;
+	int		j;
+
+	if (!s2)
+		return (NULL);
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	k = (char *) malloc (ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!k)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		k[j++] = s1[i++];
+	l = 0;
+	while (s2[l])
+		k[j++] = s2[l++];
+	k[j] = '\0';
+	free(s1);
+	return (k);
+}
+
+char	*ft_substr(char *s,int start, size_t len)
+{
+	char	*p;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (len > ft_strlen(&s[start]))
+		p = malloc((ft_strlen(&s[start]) + 1) * sizeof(char));
+	else
+		p = malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		start = ft_strlen(s);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		p[i] = s[start + i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
